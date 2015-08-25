2ac9566401300e283268781fcaef73f86d514991
inslab.dev <inslab.dev@gmail.com>
2015-08-25 18:28:35 +0900

    change sonarqube version to 5.1.2, add configuration volume and add ldap setting

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 sonarqube-docker
 ================
+Environmental Variables
+-----------------------
+LDAP_URI : URI of LDAP server (ex : ldap://111.222.333.444:389)
+
+LDAP_BASE_DN : Base DN (ex : dc=example,dc=co)
--- start.sh
+++ start.sh
@@ -7,7 +7,37 @@ cat /opt/sonar/bin/linux-x86-64/createdb.sql
 mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USERNAME} -p${DB_PASSWORD} < /opt/sonar/bin/linux-x86-64/createdb.sql
 
 if [ ! -d "/opt/sonar/extensions/plugins" ]; then
-	mv /tmp/sonar/* /opt/sonar/extensions
+    echo "Copying plugins..."
+    cp -rf /tmp/sonar/extensions /opt/sonar/
+    rm -rf /tmp/sonar/extensions
+    cd /opt/sonar/extensions/plugins && \
+        curl -O http://downloads.sonarsource.com/plugins/org/codehaus/sonar-plugins/sonar-ldap-plugin/1.4/sonar-ldap-plugin-1.4.jar
+fi
+
+if [ ! -f "/opt/sonar/conf/sonar.properties" ]; then
+    echo "Copying configuration..."
+    cp -rf /tmp/sonar/conf /opt/sonar/
+fi
+
+if [ -n "$LDAP_URI" ] && [ -n "$LDAP_BASE_DN" ]
+then
+    echo "--> LDAP setting..."
+    CONF_FILE=/opt/sonar/conf/sonar.properties
+    sed -i '/^sonar.security.realm/d' $CONF_FILE
+    sed -i '/^sonar.security.savePassword/d' $CONF_FILE
+    sed -i '/^ldap.url/d' $CONF_FILE
+    sed -i '/^ldap.user/d' $CONF_FILE
+    sed -i '/^ldap.group/d' $CONF_FILE
+    echo -e "\n\n" >> $CONF_FILE
+    echo "sonar.security.realm=LDAP" >> $CONF_FILE
+    echo "sonar.security.savePassword=false" >> $CONF_FILE
+    echo "ldap.url=$LDAP_URI" >> $CONF_FILE
+    echo "ldap.user.baseDn=$LDAP_BASE_DN" >> $CONF_FILE
+    echo "ldap.user.request=(&(objectClass=PosixAccount)(uid={login}))" >> $CONF_FILE
+    echo "ldap.user.realNameAttribute=cn" >> $CONF_FILE
+    echo "ldap.user.emailAttribute=mail" >> $CONF_FILE
+else
+    echo "Skip LDAP setting..."
 fi
 
 /opt/sonar/bin/linux-x86-64/sonar.sh console
